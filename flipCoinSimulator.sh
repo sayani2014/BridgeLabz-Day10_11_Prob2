75e75ba368efba7c33c16fd6713454425938ad4a
sayani2014 <sayanikoley8@gmail.com>
2021-05-26 12:51:16 +0530

    Flipping the coin 21 times and display the winning or tie result

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,19 +3,31 @@
 countHead=0
 countTail=0
 i=1
-read -p "Enter the number of times you want to flip the coin: " number
 
-for((i=1;i<=number;i++))
+while [ "$i" -le 21 ];
 do
 	flipCoin=$(( RANDOM%2 ))
-	echo $flipCoin
-	if(($flipCoin==1))
-	then
+	if [ $flipCoin -eq 1 ]
+	then 	
 		countHead=$(($countHead+1))
-		
-	else
+	else	
 		countTail=$(($countTail+1))
 	fi
+	if(($i==21))
+	then
+		if (( $countHead>=21 ))
+		then
+			echo "Number that reached the count of 21 maximum times is HEAD"
+		elif (( $countTail>=21 ))
+		then
+			echo "Number that reached the count of 21 maximum times is TAIL"
+		elif (( $countHead>=21 && $countTail>=21 ))
+		then
+			echo "Its a tie"
+		else
+			i=0
+			continue
+		fi
+	fi
+	let "i += 1"
 done
-echo "HEAD occurred: $countHead"
-echo "TAIL occurred: $countTail"
