f65cbef143981dba12625d9935cabdc98b923435
sayani2014 <sayanikoley8@gmail.com>
2021-05-26 12:43:02 +0530

    Flipping a coin multiple times and display the count

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,21 @@
 #! /bin/bash -x
 
-flipCoin=$(( RANDOM%2 ))
-if(($flipCoin==1))
-then
-	echo "It's a HEAD"
-else
-	echo "It's a TAIL"
-fi
+countHead=0
+countTail=0
+i=1
+read -p "Enter the number of times you want to flip the coin: " number
+
+for((i=1;i<=number;i++))
+do
+	flipCoin=$(( RANDOM%2 ))
+	echo $flipCoin
+	if(($flipCoin==1))
+	then
+		countHead=$(($countHead+1))
+		
+	else
+		countTail=$(($countTail+1))
+	fi
+done
+echo "HEAD occurred: $countHead"
+echo "TAIL occurred: $countTail"
